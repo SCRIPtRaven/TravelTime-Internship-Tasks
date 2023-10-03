d6b4ae14604b5e7af2b2e77f4b21b158fc3c60c7
Aivaras <pitcherspacegum@gmail.com>
2023-10-03 22:21:17 +0300

    Laid out plans for the rest of the task

--- location-region/src/main/scala/Main.scala
+++ location-region/src/main/scala/Main.scala
@@ -1,13 +1,17 @@
+// I have chosen the uPickle library for JSON file parsing because it has excellent case class support
 import upickle.default._
 import os._
 
 object Main extends App {
   case class Location(name: String, coordinates: (Double, Double))
   case class Region(name: String, coordinates: Seq[Seq[(Double, Double)]])
+  case class MatchedRegions(regionName: String, locationName: String)
 
   implicit val locationRw: ReadWriter[Location] = macroRW
   implicit val regionRw: ReadWriter[Region] = macroRW
   
+  // TODO: Look into ways to combine the two parsing functions into one
+  // Options: Either clause, sealed trait, generics
   def parseLocationsJson(file: String): Seq[Location] = {
     val jsonStr: String = os.read(os.pwd / file)
     val locations: Seq[Location] = upickle.default.read[Seq[Location]](jsonStr)
@@ -20,7 +24,16 @@ object Main extends App {
     val regions: Seq[Region] = upickle.default.read[Seq[Region]](jsonStr)
 
     regions
-  } 
+  }
+
+  def matching(locations: Seq[Location], regions: Seq[Region]): Seq[MatchedRegions] = {
+    // TODO: Implement
+    ???
+  }
+
+  def outputToJson(file: String, results: Seq[MatchedRegions]): Unit = {
+    // TODO: Implement
+  }
 
   val locations = parseLocationsJson("locations.json")
   val regions = parseRegionsJson("regions.json")
